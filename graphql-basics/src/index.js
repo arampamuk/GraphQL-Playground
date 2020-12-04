457228a1334ed87537b84b632edd172ec733a56f
Aram <Aram>
2020-12-04 20:13:17 +0000

    simple add and subtract  functions

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,7 +1,10 @@
 import myCurrentLocation, { getGreeting, message, name } from './myModule'
+import myAddFunction, { subtract } from './math'
 
 
 console.log(message);
 console.log(name);
 console.log(myCurrentLocation);
 console.log(getGreeting('Gozde'));
+console.log(myAddFunction(2,-5));
+console.log(subtract(55,4));
