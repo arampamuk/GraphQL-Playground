b5d932a2291239b2f25f3ca92ea596c587c28621
Aram <Aram>
2020-12-04 20:41:11 +0000

    added example scheme and resolvers for graphQL yoga!

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,38 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type definations (scheme)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        about: String!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is hello query!';
+        },
+        name() {
+            return 'Aram Pamuk';
+        },
+        location() {
+            return 'London';
+        },
+        about() {
+            return 'This is an example for graphQL yoga!'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log('The server is up!');
+});
