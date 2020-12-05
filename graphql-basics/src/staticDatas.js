a08b61624e01a058ac7116b886b40c5ef1643e79
Aram <Aram>
2020-12-05 16:08:06 +0000

    static datas separated to share

---
+++ graphql-basics/src/staticDatas.js
@@ -0,0 +1,39 @@
+const userDatas = [{
+    id: '1',
+    name: 'Aram',
+    email: 'Aram@test.com',
+    age: 10
+}, {
+    id: '2',
+    name: 'Gozde',
+    email: 'Bill@test.com',
+    age: 12
+}, {
+    id: '3',
+    name: 'Will',
+    email: 'Will@test.com',
+    age: 55
+}];
+
+const postDatas = [{
+    id: '10',
+    title: 'Lorem Ipsum is simply dummy text',
+    body: 'printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+    published: true,
+    author: '1'
+}, {
+    id: '11',
+    title: 'It is a long established fact that a reader',
+    body: 'will be distracted by the readable content of a page when looking at its layout.',
+    published: true,
+    author: '2'
+}, {
+    id: '12',
+    title: 'There are many variations of passages',
+    body: 'of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected',
+    published: true,
+    author: '3'
+}
+];
+
+export { postDatas, userDatas }
