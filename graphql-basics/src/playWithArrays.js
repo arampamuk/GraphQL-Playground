3f3c6dd9c5b1a9d0db462580c7024e4930b5e3aa
Aram <Aram>
2020-12-05 16:08:31 +0000

    added Resolver method example

--- graphql-basics/src/playWithArrays.js
+++ graphql-basics/src/playWithArrays.js
@@ -1,39 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga';
-
-const users = [{
-    id: '1',
-    name: 'Aram',
-    email: 'Aram@test.com',
-    age: 10
-}, {
-    id: '2',
-    name: 'Gozde',
-    email: 'Bill@test.com',
-    age: 12
-}, {
-    id: '3',
-    name: 'Will',
-    email: 'Will@test.com',
-    age: 55
-}];
-
-const posts = [{
-    id: '10',
-    title: 'Lorem Ipsum is simply dummy text',
-    body: 'printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-    published: true
-}, {
-    id: '11',
-    title: 'It is a long established fact that a reader',
-    body: 'will be distracted by the readable content of a page when looking at its layout.',
-    published: true
-}, {
-    id: '12',
-    title: 'There are many variations of passages',
-    body: 'of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected',
-    published: true
-}
-];
+import { postDatas, userDatas } from './staticDatas'
 
 // Type definition (scheme)
 const typeDefs = `
@@ -56,6 +22,7 @@ const typeDefs = `
         title: String!
         body: String!
         published: Boolean!
+        author: User!
     }
 `;
 
@@ -76,24 +43,31 @@ const resolvers = {
         },
         users(parent, args, ctx, info) {
             if (!args.query) {
-                return users;
+                return userDatas;
             }
-
-            return users.filter((user) => {
+z
+            return userDatas.filter((user) => {
                 return user.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase());
             })
         },
         posts(parent, args, ctx, info) {
             if(!args.query) {
-                return posts;
+                return postDatas;
             }
 
-            return posts.filter((post) => {
+            return postDatas.filter((post) => {
                 const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase());
                 const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase());
                 return isTitleMatch || isBodyMatch;
             });
-          
+        }
+    },
+    //Resolver method
+    Post: {
+        author(parent, args, ctx, info) {
+            return userDatas.find((user) => {
+                return user.id === parent.author
+            });
         }
     }
 }
