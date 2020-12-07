d99339706fa065515de0370f4d0e0251c5599679
Aram <Aram>
2020-12-07 14:27:27 +0000

    subscriptions added post, insert, update and deleted

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -77,28 +77,42 @@ const Mutation = {
         }
 
         db.post.push(post);
-        pubsub.publish(`post ${args.data.author}`, { post });
 
         if (args.data.published) {
-            pubsub.publish(`post`, { post });
+            pubsub.publish(`post`, { 
+                post: {
+                    mutation: 'CREATED',
+                    data: post
+                }
+             });
         }
 
         return post;
     },
-    deletePost(parent, args, { db }, info) {
+    deletePost(parent, args, { db, pubsub }, info) {
         const postIndex  = db.post.findIndex((post) => post.id === args.id);
 
         if (postIndex === -1) {
             throw new Error('Post not found');
         }
 
-        const deletedPosts = posts.splice(postIndex, 1);
+        const [ post ] = db.post.splice(postIndex, 1);
 
-        return deletedPosts[0];
+        if (post.published) {
+            pubsub.publish(`post`, { 
+                post: {
+                    mutation: 'DELETED',
+                    data: post
+                }
+             });
+        }
+
+        return post;
     },
-    updatePost(parent, args, { db }, info) {
+    updatePost(parent, args, { db, pubsub }, info) {
         const { id, data } = args;
         const post = db.post.find((post) => post.id === id);
+        const originalPost = { ...post }
 
         if (!post) {
             throw new Error('Post not found');
@@ -114,6 +128,33 @@ const Mutation = {
 
         if (typeof data.published === 'boolean') {
             post.published = data.published;
+
+
+            if (originalPost.published && !post.published) {
+                // deleted
+                pubsub.publish(`post`, { 
+                    post: {
+                        mutation: 'DELETED',
+                        data: originalPost
+                    }
+                 });
+            } else if(!originalPost.published && post.published) {
+                // created
+                pubsub.publish(`post`, { 
+                    post: {
+                        mutation: 'CREATED',
+                        data: post
+                    }
+                 });
+            }
+        } else if(post.published) {
+            // updated
+            pubsub.publish(`post`, { 
+                post: {
+                    mutation: 'UPDATED',
+                    data: post
+                }
+             });
         }
 
         return post;
--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -1,13 +1,7 @@
 const Subscription = {
     post: {
-        subscribe(parent, { userId }, { db, pubsub }, info) {
-            const user = db.user.find((user) => user.id === userId);
-
-                if (!user) {
-                    throw new Error('User not found');
-                }
-
-                return pubsub.asyncIterator(`post ${userId}`);
+        subscribe(parent, args, { pubsub }, info) {
+                return pubsub.asyncIterator(`post`);
             }
         }
 
