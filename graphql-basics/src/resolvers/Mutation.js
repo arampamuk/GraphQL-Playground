d70fc4c9395afe93c1f22d23a157f33723434bd4
Aram <Aram>
2020-12-07 00:51:48 +0000

    added update post resolver and graph

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -90,6 +90,28 @@ const Mutation = {
         const deletedPosts = posts.splice(postIndex, 1);
 
         return deletedPosts[0];
+    },
+    updatePost(parent, args, { db }, info) {
+        const { id, data } = args;
+        const post = db.post.find((post) => post.id === id);
+
+        if (!post) {
+            throw new Error('Post not found');
+        }
+
+        if (typeof data.title === 'string') {
+            post.title = data.title;
+        }
+
+        if (typeof data.body === 'string') {
+            post.body = data.body;
+        }
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published;
+        }
+
+        return post;
     }
 }
 
